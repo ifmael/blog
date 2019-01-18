5519706e0b04d57dfd77829ca0976294d1c01703
Ismael Rodriguez Moreno <ismael.rm.1986@gmail.com>
2019-01-18 07:43:15 +0100

    Add vertically element in menu

--- src/components/header.js
+++ src/components/header.js
@@ -8,24 +8,38 @@ const Header = ({ siteTitle }) => (
     <div class="hero-body is-paddingless">
       <div className="columns">
         <div className="column is-8 is-offset-2">
-          <nav className="navbar" role="navigation">
+          <nav className="navbar" role="navigation" ariaLabel="main navigation">
             <div className="navbar-brand">
-              <div className="navbar-item is-paddingless">
+              <div className="navbar-item">
                 <h1>ifmael</h1>
               </div>
-              <a role="button" className="navbar-burguer burguer" dataTarget="navbarBasicExample">
-                  <span ariaHidden="true" />
-                  <span ariaHidden="true" />
-                  <span ariaHidden="true" />
-                </a>
+              <a role="button" className="navbar-burger burger" aria-label="menu" ariaExpanded="false" dataTarget="navbarBasic">
+                <span ariaHidden="true"></span>
+                <span ariaHidden="true"></span>
+                <span ariaHidden="true"></span>
+              </a>
             </div>
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div id="navbarBasic" className="navbar-menu">
               <div className="navbar-start">
               </div>
               <div className="navbar-end">
-                <a className="navbar-item">Home</a>
-                <a className="navbar-item">Documentation</a>
-                <a className="navbar-item is-paddingless">More</a>
+                <div class="navbar-item vertically">
+                  <div>
+                    <a>
+                      Search
+                    </a>
+                  </div>
+                  <div>
+                    <a>
+                      me
+                    </a>
+                  </div>
+                  <div>
+                    <a>
+                      github
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </nav>
@@ -34,71 +48,6 @@ const Header = ({ siteTitle }) => (
     </div>
   </section>
 )
-
-/*
-    <section >
-    <div classNameName="hero-body is-paddingless">
-      <div classNameName="columns">
-        <div className="column is-8 is-offset-2">
-          <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="https://bulma.io">
-                <img
-                  src="https://bulma.io/images/bulma-logo.png"
-                  width="112"
-                  height="28"
-                />
-              </a>
-
-              <a
-                role="button"
-                classNameName="navbar-burger burger"
-                ariaLabel="menu"
-                ariaExpanded="false"
-                dataTarget="navbarBasicExample"
-              >
-                <span ariaHidden="true" />
-                <span ariaHidden="true" />
-                <span ariaHidden="true" />
-              </a>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-              <div className="navbar-start">
-                <a className="navbar-item">Home</a>
-
-                <a className="navbar-item">Documentation</a>
-
-                <div className="navbar-item has-dropdown is-hoverable">
-                  <a className="navbar-link">More</a>
-
-                  <div className="navbar-dropdown">
-                    <a className="navbar-item">About</a>
-                    <a className="navbar-item">Jobs</a>
-                    <a className="navbar-item">Contact</a>
-                    <hr className="navbar-divider" />
-                    <a className="navbar-item">Report an issue</a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="navbar-end">
-                <div className="navbar-item">
-                  <div className="buttons">
-                    <a className="button is-primary">
-                      <strong>Sign up</strong>
-                    </a>
-                    <a className="button is-light">Log in</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </section>
-*/
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -110,20 +59,5 @@ Header.defaultProps = {
 
 export default Header
 
-/*   <section classNameName="hero is-warning">
-    <div classNameName="hero-body">
-      <div classNameName="container">
-        <div classNameName="level is-mobile">
-          <div classNameName="level-left">
-            <div classNameName="level-item">
-              <h1 classNameName="title">ifmael</h1>
-            </div>
-          </div>
-          <div classNameName="level-right">
-            <div classNameName="level-item"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+/*
 */
